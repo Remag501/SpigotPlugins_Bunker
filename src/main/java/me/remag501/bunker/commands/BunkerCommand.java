538db915e9c54653dea34c8fe5308057ca31a18d
Vaibhav Kumar <therealvaibhavkumar10@gmail.com>
2024-08-22 17:01:35 -0700

    Bunker home works. Bunker visit is close to working.

--- src/main/java/me/remag501/bunker/commands/BunkerCommand.java
+++ src/main/java/me/remag501/bunker/commands/BunkerCommand.java
@@ -1,5 +1,6 @@
 package me.remag501.bunker.commands;
 
+import com.onarandombox.MultiverseCore.api.MultiverseWorld;
 import me.remag501.bunker.util.Schematic;
 import com.onarandombox.MultiverseCore.MultiverseCore;
 import com.onarandombox.MultiverseCore.api.MVWorldManager;
@@ -48,8 +49,12 @@ public class BunkerCommand implements CommandExecutor {
         if (args.length > 0 && args[0].equalsIgnoreCase("home"))
             return bunkerHome(sender);
         // Allows players to visit the bunker by using the "visit" argument
+        if (args.length == 1 && args[0].equalsIgnoreCase("visit")) {
+            sender.sendMessage("Usage: /bunker visit [player]");
+            return true;
+        }
         if (args.length > 0 && args[0].equalsIgnoreCase("visit"))
-            return visit(sender); // Will need args[1] for the bunker name
+            return visit(sender, args[1]); // Will need args[1] for the player name
         if (args.length > 1 && args[0].equalsIgnoreCase("admin") && args[1].equalsIgnoreCase("add"))
             return addBunkers(Integer.parseInt(args[2]), sender); // Create more bunkers using multicore
         if (args.length > 0) {
@@ -80,41 +85,73 @@ public class BunkerCommand implements CommandExecutor {
             return true;
         }
     }
-    private boolean visit(CommandSender sender) {
-        sender.sendMessage("Visiting bunker... not implemented yet");
-        return true; // Replace with actual implementation
+    private boolean visit(CommandSender sender, String playerName) {
+        ConfigUtil config = new ConfigUtil(plugin, "bunkers.yml");
+        if (!config.getConfig().contains(playerName)) {
+            sender.sendMessage("Player does not exist or have a bunker!");
+            return true;
+        }
+        String worldName = "bunker_" + config.getConfig().getString(playerName);
+        sender.sendMessage("Visiting " + playerName + "'s bunker");
+        teleportPlayer((Player) sender, worldName);
+        return true;
     }
 
     private boolean bunkerHome(CommandSender sender) {
         // Handle the default case: no arguments or non-reload arguments
+        ConfigUtil config = new ConfigUtil(plugin, "bunkers.yml");
+        // Check if player has a bunker
+        if (!config.getConfig().contains(sender.getName())) {
+            sender.sendMessage("You do not have a bunker. Use /bunker buy to purchase one.");
+            return true;
+        }
+        // Send player teleport message
         String message = plugin.getConfig().getString("message", "Default message");
         Player player = (Player) sender;
         player.sendMessage(message);
-        return true; // Replace with actual implementation
-
-    }
-
-    private boolean addBunkers(int bunkers, CommandSender sender) {
-        ((Player) sender).sendMessage("Added " + bunkers + " bunkers.");
-        ConfigUtil config = new ConfigUtil(plugin,"bunkers.yml");
-        int totalBunkers = config.getConfig().getInt("totalBunkers");
-        totalBunkers += bunkers;
-        config.getConfig().set("totalBunkers", totalBunkers);
-        config.save();
-        // Implement world creation
-//        for (int i < 0; i < bunkers; i++) {
-//
-//        }
+        // Teleport player to their bunker
+        // Get worldname
+        String worldName = config.getConfig().getString(player.getName());
+        worldName = "bunker_" + worldName;
+        teleportPlayer(player, worldName);
         return true;
     }
 
-    private void createBunkerWorld(CommandSender sender, String worldName) {
+    private void teleportPlayer(Player player, String worldName) {
+        // Get the MVWorldManager
+        MultiverseCore multiverseCore = (MultiverseCore) Bukkit.getPluginManager().getPlugin("Multiverse-Core");
+        MVWorldManager worldManager = multiverseCore.getMVWorldManager();
+        // Get the target world
+        MultiverseWorld targetWorld = worldManager.getMVWorld(worldName);
+        // Teleport the player to the spawn location of the target world
+        Location spawnLocation = targetWorld.getSpawnLocation();
+        if (spawnLocation != null) {
+            player.teleport(spawnLocation);
+        } else {
+            player.sendMessage("Spawn location not found in world: " + worldName + ". Contact an admin for help!");
+        }
+    }
+
+    private boolean addBunkers(int bunkers, CommandSender sender) {
         // Check if schematic file exists
         File schematicFile = new File(plugin.getDataFolder(), "schematics/bunker.schem");
         if (!schematicFile.exists()) {
             ((Player) sender).sendMessage("No schematic found. Make sure you named it correctly and its placed in schematics/bunker.schem");
-            return;
+            return true;
         }
+        // Update bunker count in config
+        ConfigUtil config = new ConfigUtil(plugin,"bunkers.yml");
+        int totalBunkers = config.getConfig().getInt("totalBunkers");
+        config.getConfig().set("totalBunkers", totalBunkers + bunkers);
+        config.save();
+        // Create bunker worlds with schematic file
+        for (int i = 0; i < bunkers; i++)
+            createBunkerWorld(sender, "bunker_" + (totalBunkers + i), schematicFile);
+        ((Player) sender).sendMessage("Added " + bunkers + " bunkers.");
+        return true;
+    }
+
+    private void createBunkerWorld(CommandSender sender, String worldName, File schematicFile) {
         // Check if Multiverse-Core is installed
         Plugin multiversePlugin = Bukkit.getPluginManager().getPlugin("Multiverse-Core");
         MultiverseCore multiverseCore = (MultiverseCore) multiversePlugin;
