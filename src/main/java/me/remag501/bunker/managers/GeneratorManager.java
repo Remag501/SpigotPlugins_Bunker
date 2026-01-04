015fd7a697330bb22d5a1a3f7523b3ee715658a8
Remag501-Desktop <evanverma2003@gmail.com>
2026-01-03 16:36:51 -0800

    feat (commands): Swapped generators from kgen to nextgen plugin

--- src/main/java/me/remag501/bunker/managers/GeneratorManager.java
+++ src/main/java/me/remag501/bunker/managers/GeneratorManager.java
@@ -1,54 +1,81 @@
 package me.remag501.bunker.managers;
 
-import me.kryniowesegryderiusz.kgenerators.Main;
-import me.kryniowesegryderiusz.kgenerators.api.KGeneratorsAPI;
-import me.kryniowesegryderiusz.kgenerators.api.interfaces.IGeneratorLocation;
-import me.kryniowesegryderiusz.kgenerators.api.objects.AbstractGeneratedObject;
-import me.kryniowesegryderiusz.kgenerators.generators.generator.GeneratorsManager;
-import me.kryniowesegryderiusz.kgenerators.generators.generator.enums.GeneratorType;
-import me.kryniowesegryderiusz.kgenerators.generators.generator.objects.GeneratedBlock;
-import me.kryniowesegryderiusz.kgenerators.generators.generator.objects.Generator;
-import me.kryniowesegryderiusz.kgenerators.generators.locations.objects.GeneratorLocation;
+import com.muhammaddaffa.nextgens.NextGens;
+import com.muhammaddaffa.nextgens.api.GeneratorAPI;
+import com.muhammaddaffa.nextgens.generators.ActiveGenerator;
+import com.muhammaddaffa.nextgens.generators.Generator;
 import me.remag501.bunker.Bunker;
 import me.remag501.bunker.core.BunkerInstance;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.Plugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
+
+import static io.papermc.lib.PaperLib.getChunkAtAsync;
 
 public class GeneratorManager {
 
     public static void createGenerator(Player player, World world, BunkerInstance bunkerInstance) {
+        // 1. Get the NextGens API/Instance
+        NextGens nextGensPlugin = NextGens.getInstance();
+        if (nextGensPlugin == null) {
+            Bukkit.getLogger().warning("[Bunker] NextGens instance is null!");
+            return;
+        }
+
+        // 2. Get the GeneratorManager from NextGens
+        com.muhammaddaffa.nextgens.generators.managers.GeneratorManager manager = nextGensPlugin.getGeneratorManager();
+
         List<BunkerInstance.GeneratorInfo> generators = bunkerInstance.getGenerators();
-        if (generators == null || generators.isEmpty()) return;
+        if (generators == null || generators.isEmpty()) {
+            return;
+        }
 
         for (BunkerInstance.GeneratorInfo info : generators) {
             String type = info.type;
-            Location loc = info.location;
+            Location loc = info.location.clone(); // Clone to prevent modifying the original reference
             loc.setWorld(world);
 
-            Generator generator = Main.getGenerators().get(type);
+            // 3. Find the generator template
+            Generator generator = manager.getGenerator(type);
             if (generator == null) {
-                Bukkit.getLogger().warning("Generator type '" + type + "' not found.");
+                Bukkit.getLogger().warning("[Bunker] Generator type '" + type + "' not found in NextGens!");
                 continue;
             }
 
-            GeneratorLocation genLoc = new GeneratorLocation(
-                    -1,
-                    generator,
-                    loc,
-                    Main.getPlacedGenerators().new ChunkInfo(loc.getChunk()),
-                    Main.getPlayers().getPlayer(player.getName()),
-                    null
-            );
+            // 4. Use Native Paper API for Async Chunk Loading (Replaces PaperLib)
+            // This fixes the NoClassDefFoundError
+            world.getChunkAtAsync(loc).thenAccept(chunk -> {
+                Block block = loc.getBlock();
 
-            genLoc.placeGenerator(player, true);
-            Bukkit.getLogger().info("Placed generator '" + type + "' at " + loc);
+                try {
+                    // 5. Register the generator
+                    // Using the player as the owner so it counts towards their limits/stats
+                    ActiveGenerator activeGenerator = manager.registerGenerator(player, generator, block);
+                    double mainTimer = activeGenerator.getTimer();
+                    activeGenerator.setTimer(20); // Make it spawn instantly
+                    new BukkitRunnable() {
+
+                        @Override
+                        public void run() {
+                            activeGenerator.setTimer(mainTimer);
+                        }
+                    }.runTaskLater(Bukkit.getPluginManager().getPlugin("Bunker"), 20);
+
+                    activeGenerator.setTimer(20);
+                    activeGenerator.setCorrupted(false);
+
+
+                    Bukkit.getLogger().info("[Bunker] Registered " + type + " at " + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ());
+                } catch (Exception e) {
+                    Bukkit.getLogger().severe("[Bunker] Failed to register generator: " + e.getMessage());
+                    e.printStackTrace();
+                }
+            });
         }
     }
 
