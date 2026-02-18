fc12a07db8c83220fa4d8617e5bf1b23f1b88f06
Remag501-Desktop <evanverma2003@gmail.com>
2026-02-17 18:03:31 -0800

    fix: added DI for all remaining objects within main class. (This build compiles)

--- src/main/java/me/remag501/bunker/Bunker.java
+++ src/main/java/me/remag501/bunker/Bunker.java
@@ -5,9 +5,14 @@ import me.remag501.bunker.commands.BunkerCommand;
 //import me.remag501.bunker.commands.BunkerCommandOld;
 import me.remag501.bunker.listeners.GeneratorBreakListener;
 import me.remag501.bunker.listeners.OpenContainer;
+import me.remag501.bunker.managers.AdminManager;
 import me.remag501.bunker.managers.BunkerCreationManager;
 import me.remag501.bunker.managers.BunkerConfigManager;
 import me.remag501.bunker.managers.ConfigManager;
+import me.remag501.bunker.service.GeneratorService;
+import me.remag501.bunker.service.HologramService;
+import me.remag501.bunker.service.NPCService;
+import me.remag501.bunker.service.SchematicService;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Bunker extends JavaPlugin {
@@ -15,22 +20,34 @@ public final class Bunker extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        // Create managers
-        BunkerConfigManager bunkerConfigManager = new BunkerConfigManager(this);
-        BunkerCreationManager bunkerCreationManager = new BunkerCreationManager(this, bunkerConfigManager);
-
         // Plugin startup logic
         saveDefaultConfig();
-        ConfigManager config = new ConfigManager(this, "bunkers.yml");
-        // Reload configuration
+        ConfigManager configManager = new ConfigManager(this, "bunkers.yml");
+
+        // Create services
+        HologramService hologramService = new HologramService();
+        GeneratorService generatorService = new GeneratorService(this);
+        NPCService npcService = new NPCService(this);
+        SchematicService schematicService = new SchematicService(this);
+
+        // Create managers
+        BunkerConfigManager bunkerConfigManager = new BunkerConfigManager(this);
+        BunkerCreationManager bunkerCreationManager = new BunkerCreationManager(this, configManager,
+                bunkerConfigManager, generatorService, hologramService, npcService, schematicService);
+        AdminManager adminManager = new AdminManager(this, bunkerCreationManager, hologramService, generatorService);
+
+        // Setup commands
         BunkerCommand command = new BunkerCommand(this, bunkerConfigManager, bunkerCreationManager);
         getCommand("bunker").setExecutor(command);
-        BunkerAdminCommand adminCommand = new BunkerAdminCommand(this, bunkerConfigManager,bunkerCreationManager);
+        BunkerAdminCommand adminCommand = new BunkerAdminCommand(bunkerConfigManager, bunkerCreationManager, adminManager);
         getCommand("bunkeradmin").setExecutor(adminCommand);
-        getLogger().info("Bunker has been enabled!");
+
         // Register listeners
         getServer().getPluginManager().registerEvents(new OpenContainer(), this);
         getServer().getPluginManager().registerEvents(new GeneratorBreakListener(), this);
+
+        // Send startup message
+        getLogger().info("Bunker has been enabled!");
 
     }
 
